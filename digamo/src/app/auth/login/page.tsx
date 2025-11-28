73fe44f6b220a531f4b3fd9a742f8dbd3a84b283
goofygoober <villamilrhona@gmail.com>
2025-11-28 20:09:45 +0800

    Auth-Creation
    -Moved auth to app
    -Made Login Folder

---
+++ digamo/src/app/auth/login/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+// import Link from "next/link";
+
+const login = () => {
+    return (
+        <div>helloo</div>
+    );
+};
+
+export default login;        
+{/* <div className="bg-white flex flex-col min-h-screen overflow-auto">
+
+
+<h1>HELLO!</h1>
+
+</div> */}
