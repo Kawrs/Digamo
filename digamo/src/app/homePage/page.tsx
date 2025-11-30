749ca8e06f10a9508efeb01c9611d1ca69415b53
mantaurmantaur <orng.digital@gmail.com>
2025-11-30 17:16:58 +0800

    [Resolve][Bautista]-error in homepage
    --used Suspense if ang client depends on URL

--- digamo/src/app/homePage/page.tsx
+++ digamo/src/app/homePage/page.tsx
@@ -2,13 +2,16 @@
 import HeaderHome from "components/home-page/HeaderHome";
 import DecorationLR from "components/home-page/DecorationLR";
 import Carousel from "components/home-page/CarouselFeatures";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen w-screen flex flex-col overflow-x-hidden items-center justify-center relative bg-homepage-bg bg-cover bg-center bg-no-repeat">
       <DecorationLR />
       <HeaderHome />
-      <Carousel />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Carousel />
+      </Suspense>
     </div>
   );
 }
